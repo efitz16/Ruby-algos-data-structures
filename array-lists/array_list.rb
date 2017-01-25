1b36ca1b029f09ba90a08c6ca946c5f36c82a707
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-01-24 19:59:33 -0500

    ArrayList class with private writer

--- array-lists/array_list.rb
+++ array-lists/array_list.rb
@@ -8,20 +8,34 @@ class ArrayList
   end
 
   def add(element)
-    self.array = Array.new(self.array.size + 1)
+    self.array = Array.new(self.array.length + 1)
 
-    self.array[self.array.size - 1] = element
+    self.array[self.array.length - 1] = element
 
     return element
   end
 
   def get(index)
+    check_bounds(index)
+
+    self.array[index]
   end
 
-  # def size
-  # 	self.array.length
-  # end
+  def set(index, element)
+    check_bounds(index)
+
+    self.array[index] = element
+  end
+
+  def length
+  	self.array.size
+  end
 
   private
   attr_writer :array
+
+  def check_bounds(index)
+    raise OutOfBoundsException, "Out of bounds" if index > self.array.size
+  end
+
 end
