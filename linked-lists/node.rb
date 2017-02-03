5533b39b60ecdc1e5b8efee1a2b6115be137e03d
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2017-02-03 16:22:42 -0500

    create node class

---
+++ linked-lists/node.rb
@@ -0,0 +1,16 @@
+class Node
+  attr_reader :next
+
+  def initialize(element)
+  	@element = element
+  	@next = nil
+  end
+
+  def insert_after(other_node)
+  	self.next = other_node
+  end
+
+  def remove_after()
+  	self.next = nil
+  end
+end
